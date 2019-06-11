7d8a44c1c054220c27157f05e6273ef8507f5e90
Ray <r.carroll81@gmail.com>
2019-06-11 09:16:35 -0400

    nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'todo-list',
+    component: TodoListComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { EntryComponent } from './components/todo-list/entry/entry.component';
 import { ListComponent } from './components/todo-list/list/list.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoListComponent,
     EntryComponent,
-    ListComponent
+    ListComponent,
+    DashboardComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
